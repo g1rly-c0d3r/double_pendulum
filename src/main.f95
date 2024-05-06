da35580aa10f3b85be8b398c14a225e99e4e99b6
g1rly-c0d3r <girly_c0d3r@aol.com>
2024-05-06 13:41:49 -0500

    variables

--- src/main.f95
+++ src/main.f95
@@ -2,9 +2,9 @@ program doublepen
   use types, only: dp
   use constants, only: PI
   implicit none
-  real(dp) :: radius1, pos1, linvel1, radvel1, linaccel1, radaccel1
-  real(dp) :: radius2, pos2, linvel2, radvel1, linaccel2, radaccel2
-  real(dp) :: angle
+  real(dp) :: radius1, theta1, dtheta1, d2theta1
+  real(dp) :: radius2, theta2, dtheta2, d2theta2
+  real(dp) :: time
 
   
 
