ef2308cbbc4b78d464c7038549d58ebdf32a309c
g1rly-c0d3r <girly_c0d3r@aol.com>
2024-05-20 15:07:38 -0500

    im not puting those eq's in yet

--- src/function.f95
+++ src/function.f95
@@ -7,19 +7,19 @@ function diffeq(t, y) result(yprime)
   integer, parameter     :: dp = kind(0.d0)
   real(dp), intent(in)   :: t
   real(dp), intent(in)   :: y(:)
-  real(dp), dimension(2) :: yprime 
-  real(dp)               :: M, RHO, CD, R, A
-  real(dp), parameter    :: pi = 3.1415926535897932384626433
-  real(dp), parameter    :: g = 9.80665
+  real(dp), dimension(4) :: yprime 
+  real(dp), parameter    :: m1 = 1 !kg
+  real(dp), parameter    :: m2 = 1 !kg
+  real(dp), parameter    :: l1 = 1 !m
+  real(dp), parameter    :: l2 = 1 !m
+  real(dp), parameter    :: g = 9.80665 !m/s**2
 
-  M = 160
-  RHO = 1.25
-  CD = 1.15 
-  R = 3.9 
-  A = pi*(R**2)
-
-  yprime(1) = y(2)
-  yprime(2) = ( (RHO * A * CD) / (2*M) * y(2)**2 ) - g
+  yprime(1) = y(3)
+  yprime(2) = y(4)
+  yprime(3) =     / &
+              
+  yprime(4) =     / &
+              
   
 end function diffeq
 
