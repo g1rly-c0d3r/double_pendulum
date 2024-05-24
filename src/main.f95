500723b26d1e82bdb8d16cf38c7aba6656317b4a
g1rly-c0d3r <girly_c0d3r@aol.com>
2024-05-24 13:09:02 -0500

    fixed lol

--- src/main.f95
+++ src/main.f95
@@ -7,15 +7,17 @@ program doublepen
   real(dp), parameter :: pi = 3.1415926535897932384626433832795_dp
 
   integer, parameter          :: m = 4
-  integer, parameter          :: N = 9
+  integer, parameter          :: N = 9999
   real(dp), dimension(N)      :: t
   real(dp), dimension(N,m)    :: y
   real(dp), dimension(5)      :: init_cond
   real(dp)                    :: h, theta1, theta2, omega1, omega2, tnot, a, b
   real(dp), dimension(N)      :: x1, y1, x2, y2
+  real(dp)                    :: l1, l2
 
   integer                     :: fstream
-  character(len=100)           :: filename
+  character(len=100)          :: filename
+  character(len=4)            :: filenum
 
 
   integer :: i, j
@@ -25,26 +27,31 @@ program doublepen
   h = real(b - a, dp) / N
   
   tnot = 0.0_dp
-  theta1 = pi/4
-  theta2 = 0.0_dp
+  theta1 = 1e-12_dp
+  theta2 = 0.1_dp
   omega1 = 0.0_dp
   omega2 = 0.0_dp
+  l1 = 1.0_dp
+  l2 = 1.0_dp
 
   init_cond = [theta1,theta2,omega1,omega2,tnot]
 
   CALL sys_RK4(diffeq, init_cond, y, t, h, m)
 
-  x1 = cos(y(:,1) - pi/2)
-  y1 = sin(y(:,1) - pi/2)
+  x1 = l1 * cos(y(:,1) - pi/2)
+  y1 = l1 * sin(y(:,1) - pi/2)
 
-  x2 = x1 + cos(y(:,2) - pi/2) 
-  y2 = y1 + sin(y(:,2) - pi/2)
+  x2 = x1 + l2 * cos(y(:,2) - pi/2) 
+  y2 = y1 + l2 * sin(y(:,2) - pi/2)
 
 do i = 1, N
-  filename = "target/data/pos_"//char(i + 48)//".dat"
+  filenum = char(mod(i/1000, 10) + 48)//char(mod(i/100, 10) + 48)//char(mod(i/10, 10) + 48)//char(mod(i, 10) + 48)
+  filename = "target/data/pos_"// filenum //".dat"
   open(newunit=fstream, file=filename, status="replace", action="write")
 
-  write(fstream, *) x1(i), y1(i), x2(i), y2(i)
+  write(fstream, *) 0, 0
+  write(fstream, *) x1(i), y1(i)
+  write(fstream, *) x2(i), y2(i)
   close(fstream)
 end do
 
