68681e878184eb08ba9617a0917273bed0fffe9b
g1rly-c0der <girly_c0d3r@aol.com>
2024-05-28 20:59:31 -0500

    fixed framerate

--- src/gen_vid.bash
+++ src/gen_vid.bash
@@ -1,3 +1,3 @@
 cd target/data
 
-ffmpeg -y -framerate 1000 -pattern_type glob -i '*.png' -c:v libx264 -r 200 double_pendulum.mp4
+ffmpeg -y -framerate 100 -pattern_type glob -i '*.png' -c:v libx264 -r 200 double_pendulum.mp4
